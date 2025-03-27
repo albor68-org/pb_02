66740428afdb6cad36646e64e01b6e75b3016e39
Novikov <novikovpav2015@yandex.ru>
2025-03-27 09:21:38 +0300

    coml ch03

--- med.cpp
+++ med.cpp
@@ -9,7 +9,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::setprecision;
-using std::sort
+using std::sort;
 using std::streamsize;
 using std::string;
 using std::vector;
@@ -18,7 +18,7 @@ int main () {
 	// Запрашиваем имя студента и выводим приветствие
 	cout << "Пожалуйста, введите ваше имя: ";
 	string name;
-	cin >> name
+	cin >> name;
 	cout << "Здравствуй, " << name << "!" << endl;
 
 	cout << "Введите ваши оценки за контрольную работу и экзамен: ";
@@ -31,11 +31,11 @@ int main () {
 	vector<double> hamework;  // Динамический массив (вектор) для оценок за СР
 	double x;
 	while (cin >> x)
-		homework.push_back(x);
+		hamework.push_back(x);
 
 	// Проверяем количество введеных оценок за СР (должно быть больше 0)
 	typedef vector<double>::size_type vec_sz;
-	vec_sz size = homework.size();
+	vec_sz size = hamework.size();
 	if (size == 0) {
 		cout << endl << "Вы должны ввести оценки за самостоятельную работу (хотя бы одну).  "
 		                "Попробуйте еще раз." << endl;	
@@ -43,18 +43,18 @@ int main () {
 	}
 
 	// Сортируем оценки за СР
-	sort(homework.begin() homework.end());
+	sort(hamework.begin(), hamework.end());
 
 	// Вычисляем медиану оценок за СР
 	vec_sz mid = size/2;
 	double median;
-	median = size % 2 == 0 ? (homework[mid] + homework[mid-1]) / 2
-	                       : homework(mid);
+	median = size % 2 == 0 ? (hamework[mid] + hamework[mid-1]) / 2
+	                       : hamework[mid];
 
 	// Выводим результат, вычисляя его прямо в процессе вывода
 	streamsize prec = cout.precision();
 	cout << name << ", ваша итоговая оценка: " << setprecision(3)
-	     << 0.3 * midterm + 0.4 * final + 0.4 * median
+	     << 0.2 * midterm + 0.4 * final + 0.4 * median
 	     << setprecision(prec) << endl;
 
 	return 0;
