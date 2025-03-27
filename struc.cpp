dd1877d37decdd8de2d420c4cbf276a29a556b08
Евгений Козлов <j.kozlov2006ayandex.ru@gmail.com>
2025-03-27 11:03:19 +0300

    lab4

--- struc.cpp
+++ struc.cpp
@@ -82,6 +82,10 @@ istream& read_hw (istream& in, vector<double>& hw) {
 
 		in.clear();
 	}
+
+	char b;
+	in >> b;
+
 	return in;
 }
 
@@ -114,7 +118,7 @@ int main () {
 	// Считываем данные об аттестации студентов, сохраняя их в векторе (по записи на каждого студента)
     // При считывании вычисляем длину имени и ищем максимальную
 	while (read(cin, record)) {
-		maxlen = max(maxlen, record.name.size());
+		maxlen = max(maxlen, record.name.size() / 2);
 		students.push_back(record);
 	}
 
@@ -126,7 +130,7 @@ int main () {
 
 		// Выводим имя студента и пробелы для выравнивания оценок
 		cout << students[i].name
-		     << string(maxlen + 1 - students[i].name.size(), ' ');
+		     << string(maxlen + 4 - students[i].name.size() / 2, ' ');
 
 		// Вычисляем (если получится) и выводим итоговую оценку
 		try {
