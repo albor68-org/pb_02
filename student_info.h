a0927958ab804d256a284e8666de819c3540b925
Нестеров Андрей <And2005.rf@yandex.ru>
2025-03-31 12:03:02 +0300

    Лабороторная работа 5 сделана

---
+++ student_info.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct Student_info {
+	std::string name;
+	double midterm;
+	double final;
+	std::vector<double> homework;
+};
+
+
+std::istream& read_hw (std::istream& in, std::vector<double>& hw);
+std::istream& read (std::istream& is, Student_info& s);
+bool compare (const Student_info& x, const Student_info& y);
