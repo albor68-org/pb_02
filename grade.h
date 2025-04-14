ac2080398e66fa383344647bc8d19c3d9aaaa686
Pavel <novikovpav2015@yandex.ru>
2025-04-14 12:58:21 +0300

    ЛР 6

--- grade.h
+++ grade.h
@@ -6,10 +6,13 @@
 #include <vector>
 
 
+
 double grade(double midterm, double final, double homework);
 
 double grade(double midterm, double final, const std::vector<double>& hw);
 
 double grade(const Student_info& s);
+
+bool f_grade(double grade);
 
 #endif // GRADE_H
